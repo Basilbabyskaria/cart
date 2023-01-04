64df974a0db163a668151291479801779ae52be6
phoenix <75119183+Basilbabyskaria@users.noreply.github.com>
2023-01-04 21:48:29 +0530

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AllProductsComponent } from './products/all-products/all-products.component';
+import { CartComponent } from './products/cart/cart.component';
+import { WishListComponent } from './products/wish-list/wish-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
